cc4f7f6e2ca34335952bf55637e8561d61ad770f
01099473080 <01099473080@naver.com>
2019-04-09 17:59:12 +0900

    2019.04.09 17:59 final

--- seoul_cctv/pop.py
+++ seoul_cctv/pop.py
@@ -9,11 +9,31 @@ import folium
 #print(xls.head())
 
 ctx='../data/'
-pop_data= ctx+ 'population_in_Seoul.xls'
-cctv_data = ctx +'CCTV_in_Seoul.csv'
-aa = pd.read_excel(pop_data)
-xls = pd.read_csv(cctv_data)
+xls = pd.read_excel(ctx+ 'population_in_Seoul.xls'
+                    ,encoding='UTF-8'
+                    ,header=2
+                    ,usecols='B,D,G,J,N')
+csv = pd.read_csv(ctx +'CCTV_in_Seoul.csv')
 
-print(aa.head())
-print(xls.head())
+pop_data = xls
+cctv_data = csv
 
+cctv_data_schema = cctv_data.columns
+pop_data_schema = pop_data.columns
+
+""""""
+['구별', '소계', '2013년도 이전', '2014년', '2015년', '2016년']
+
+'자치구', '계', '계.1', '계.2', '65세이상고령자', '65세이상고령자'
+""""""
+
+cctv_data.rename(columns={cctv_data.columns[0]:'구별'},inplace=True)
+# inplace=True 실제 변수의 내용을 바꿔라
+
+pop_data.rename(columns={pop_data.columns[0]:'구별'
+                         ,pop_data.columns[1]:'인구수'
+                         ,pop_data.columns[2]:'한국인'
+                         ,pop_data.columns[3]:'외국인'
+                         ,pop_data.columns[4]:'고령자'}
+                    ,inplace=True)
+print(pop_data.columns)
